bc5babe76f790380f4f52259996131841655946e
Zhang Chao <33809073@qq.com>
2016-09-06 02:06:27 +0800

    修复压缩bug

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+/**
+ * Created by zc on 2016/9/6.
+ */
+var gulp=require("gulp")
+var html=require("gulp-htmlmin")
+var jsmin=require("gulp-uglify")
+var pics=require("gulp-imagemin")
+var cssmin=require("gulp-clean-css")
+
+gulp.task("html",function(){
+    gulp.src("index.html")
+        .pipe(html({collapseWhitespace: true,
+            removeComments: true
+        }))
+        .pipe(gulp.dest("js"))
+
+})
+gulp.task("jsmin",function(){
+    gulp.src("js/*.js")
+        .pipe(jsmin())
+        .pipe(gulp.dest("jsmin"))
+
+})
+gulp.task("pics",function(){
+    gulp.src("images/*.*")
+        .pipe(pics())
+        .pipe(gulp.dest("imagesmin"))
+
+})
+gulp.task("cssmin",function(){
+    gulp.src("css/*.css")
+        .pipe(cssmin())
+        .pipe(gulp.dest("cssmin"))
+
+})
