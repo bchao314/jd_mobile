a0c1d850432b4e6d6133b35d14dafdc37acf6147
Zhang Chao <33809073@qq.com>
2016-09-09 02:24:32 +0800

    本地图片换网络

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,6 @@
  */
 var gulp=require("gulp")
 var html=require("gulp-htmlmin")
-var jsmin=require("gulp-uglify")
-var pics=require("gulp-imagemin")
-var cssmin=require("gulp-clean-css")
 
 gulp.task("html",function(){
     gulp.src("index.html")
@@ -16,20 +13,3 @@ gulp.task("html",function(){
         .pipe(gulp.dest("js"))
 
 })
-gulp.task("jsmin",function(){
-    gulp.src("js/*.js")
-        .pipe(jsmin())
-        .pipe(gulp.dest("jsmin"))
-
-})
-gulp.task("pics",function(){
-    gulp.src("images/*.*")
-        .pipe(pics())
-        .pipe(gulp.dest("imagesmin"))
-
-})
-gulp.task("cssmin",function() {
-    gulp.src("css/*.css")
-        .pipe(cssmin())
-        .pipe(gulp.dest("cssmin")
-    )})
